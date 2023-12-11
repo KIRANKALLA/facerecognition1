cba0c177d807513d7b89c6277debbe3ef2952e36
KallaKiran <kallakiran1974@rcee.ac.in>
2023-12-11 15:59:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from deepface import DeepFace
+import cv2
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+st.header('AI&DS::RCEE')
+st.title('Face Recognition')
+
+image = st.file_uploader('Take any image')
+
+if image:
+    img = Image.open(image)
+    st.image(img)
+    img = np.array(img)
+    objs = DeepFace.find(img,'rcefaces/database',model_name='Facenet512')
+    name = objs[0]['identity'].split('/')[-1].split('.')[0]
+    st.write(' Hi ' + '  name') 
+
+     
