a520bf76f4c570941cf87a65bbb678a8698cf2fb
KIRANKALLA <kallakiran1974@gmail.com>
2023-12-15 14:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import streamlit as st
+import json
 
 st.header('AI&DS::RCEE')
 st.title('Face Recognition')
@@ -16,6 +17,17 @@ if image:
     objs = DeepFace.find(img,'database',model_name='Facenet512',enforce_detection=False)
     name = str(objs[0]['identity'][0])
     name = name.split('/')[-1].split('.')[0]
-    st.write(' Hi ' + '  ' + name) 
+    st.write(' Hi ' + '  ' + name)
+    information = DeepFace.analyze(img)
+    infomation = json.load(information)
+    age = information['age']
+    emotion = information['dominant_emotion']
+    race = information['dominant_race']
+    gender = information['gender']
+    st.write('Your age is   ',age)
+    st.write('You are  ',gender)
+    st.write('You are from  ', race)
+    st.write('You are feeling very ', emotion)
+    
 
      
